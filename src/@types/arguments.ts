f78b4eb02ce904ff770cad0163bb9036bba06be0
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2024-11-20 15:57:49 -0300

    Refatorar: alterar estrutura do código

--- src/@types/arguments.ts
+++
@@ -1,4 +0,0 @@
-export enum ArgumentsTypes {
-  SITE_NAME = '--site-name',
-  TEMPLATE_URL = '--template-url',
-}
--- src/webcrafter.ts
+++
@@ -1,5 +0,0 @@
-export class WebCrafter {
-  public static async downloadFrom(url: string): Promise<string> {
-    return fetch(url).then((resp) => resp.text());
-  }
-}
